aca11de02036b458f1fcfcb6011c9690e1860f70
romirant <robertmirante@gmail.com>
2023-12-22 11:19:51 +0000

    edited add items

--- pet_shopping_cart.py
+++ pet_shopping_cart.py
@@ -4,14 +4,15 @@
 '''
 
 
-def add_item(x,y):
+def add_item(x, p_dict):
     """Add items to my cart."""
+    for key, value in p_dict.items():
+        print(f"{key}:\t£{value}")
     item = input("Please enter the name of the item you would like to add: ")
-    cost = float(input("Please enter the price of that item: £"))
+    #cost = float(input("Please enter the price of that item: £"))
     x.append(item)
-    y.append(cost)
-    print(f"\n{item} has been added to your cart successfully!")
-    print("-"*80)
+    #y.append(cost)
+    print(f"\n{item} has been added to your cart successfully!\n" + "-"*80)
 
 
 def view_items(x):
@@ -34,6 +35,15 @@ def sort_items(x):
     x.sort()
     print("List sorted!")
 
+def total_cost(x,p_dict):
+    '''Find total of cart'''
+    total_price = 0
+    for item in x:
+        total_price += p_dict.get(item)
+    return total_price
+        
+
+
 
 
 items = ["Cat Food", "Dog Food", "Dog Bed", "Toys"]
@@ -43,6 +53,10 @@ cart = []
 cost_list = []
 border = "-"*80
 welcome = "Welcome to the Pets-R-Us Shop!"
+
+
+
+
 
 print(border)
 print(welcome)
@@ -55,21 +69,38 @@ while True:
                 "\n3: Delete item\n4: Sort List\n0: Exit\n"\
                     "\nI would like to: ")
     print(border)
-    if menu == "1":
-        add_item(cart,cost_list)
-    elif menu == "2":
-        view_items(cart)
-    elif menu == "3":
-        delete_item(cart)
-    elif menu == "4":
-        sort_items(cart)
-    elif menu == "0":
-        # Exit program
-        print("Goodbye!")
-        break
-    else:
-        print("\n")
-        print("That is not a valid option")
-        print("-"*80)
+    # if menu == "1":
+    #     add_item(cart,cost_list)
+    # elif menu == "2":
+    #     view_items(cart)
+    # elif menu == "3":
+    #     delete_item(cart)
+    # elif menu == "4":
+    #     sort_items(cart)
+    # elif menu == "0":
+    #     # Exit program
+    #     print("Goodbye!")
+    #     break
+    # else:
+    #     print("\nThat is not a valid option\n" + "-"*80)
+    #     print("-"*80)
 
 
+
+
+
+    match menu:
+        case "1":
+            add_item(cart, prices)
+        case "2":
+            view_items(cart)
+        case "3":
+            delete_item(cart)
+        case "4":
+            sort_items(cart)
+        case "0":
+            #Exit program
+            print("Goodbye!")
+            break
+        case _:
+            print("\nThat is not a valid option\n" + "-"*80)
